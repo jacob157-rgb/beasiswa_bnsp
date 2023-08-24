1773dc96d8a639bc97d44b4496b4449897bde3d1
Jacob Jockey Saputra <jacobjokey@gmail.com>
2023-08-24 14:11:39 +0700

    update

--- beasiswa.sql
+++ beasiswa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Aug 24, 2023 at 07:06 AM
+-- Generation Time: Aug 24, 2023 at 07:10 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.2.9
 
