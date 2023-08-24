0b2e2a0e0305e828d5fc3ed76242ffcfa22194b5
jacob157-rgb <jacobjokey@gmail.com>
2023-08-24 10:28:45 +0700

    menambah tabel data_beasiswa

--- beasiswa.sql
+++ beasiswa.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Aug 23, 2023 at 03:45 PM
--- Server version: 8.0.30
--- PHP Version: 8.2.9
+-- Host: localhost
+-- Generation Time: Aug 24, 2023 at 05:28 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,19 +24,50 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `data_beasiswa`
+--
+
+CREATE TABLE `data_beasiswa` (
+  `id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `nohp` int(20) NOT NULL,
+  `sem` int(1) NOT NULL,
+  `ipk` decimal(3,2) NOT NULL,
+  `jenis_beasiswa` enum('Akademik','Non Akademik') NOT NULL,
+  `berkas` varchar(255) NOT NULL,
+  `status_ajuan` enum('Belum di verifikasi','Sudah diverifikasi') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
-  `id` int NOT NULL,
-  `name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
+(2, 'Admin', 'admin@mail.com', '12');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `data_beasiswa`
+--
+ALTER TABLE `data_beasiswa`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -49,10 +80,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `data_beasiswa`
+--
+ALTER TABLE `data_beasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
